3ef1947e49880a30d610c88acf87c70a55790ff3
Chris <nady.christopher@gmail.com>
2017-08-21 14:12:52 -0500

    Add files via upload

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Christopher Nady on 8/2/17.
+//  Copyright © 2017 Nady Analytics, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct  Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero 
+}
